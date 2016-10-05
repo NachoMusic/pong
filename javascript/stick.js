17e33aa00b4b68c78d6949d5a077c36527c75de3
Nacho Valera Herrera <nachovaleraherreraes@gmail.com>
2016-10-06 01:47:15 +0200

    This is the branch develop

--- javascript/stick.js
+++ javascript/stick.js
@@ -7,25 +7,25 @@
  * @param {id_stick} ,side {left or right}
  *
  */
-function Stick(id_stick,side) {
+function Stick(id_stick, side) {
 
-  this.imgObj = document.getElementById(id_stick);
-	this.side= side || "left" ; //right,left,
-	this.gap=25;    //From this.position in pixels
-  var self = this;
+    this.imgObj = document.getElementById(id_stick);
+    this.side = side || "left"; //right,left,
+    this.gap = 25; //From this.position in pixels
+    var self = this;
 
-  window.addEventListener("mousemove",
-  function(e){
-    y= (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-    self.locate(self.gap,y);
-  }
-  ,false);
-	//Posicionem stick a les coordenades x,y
-	this.locate = function(x,y){
-		this.x=x;this.y=y;
-		this.imgObj.style[this.side] = (Math.round(x))+ 'px';
-		this.imgObj.style.top = (Math.round(y)) + 'px';
-	}
-}// End Stick class
+    window.addEventListener("mousemove",
+        function(e) {
+            y = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+            self.locate(self.gap, y);
+        }, false);
+    //Posicionem stick a les coordenades x,y
+    this.locate = function(x, y) {
+        this.x = x;
+        this.y = y;
+        this.imgObj.style[this.side] = (Math.round(x)) + 'px';
+        this.imgObj.style.top = (Math.round(y)) + 'px';
+    }
+} // End Stick class
 
 module.exports = Stick;
