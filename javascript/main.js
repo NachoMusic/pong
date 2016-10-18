7321d4d463f240d4f231688e035ffed1f23ac8b7
Nacho Valera Herrera <nachovaleraherreraes@gmail.com>
2016-10-18 04:44:06 +0200

    The game creates a cookie asking your username. When you open again the game, it shows your username without asking it

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,24 @@ var singletonContext = require('./patterns/singleton/singletonContext');
 
 //Once the page has been completely loaded. Including images. We start the game
 window.onload = function() {
+    // Cookie for the name
+    function checkCookie() {
+        var username=utils.getCookie("username");
+        if (username!="") {
+            // alert("Welcome again " + username);
+            showUsername(username);
+        } else {
+            username = prompt("Please enter your name:", "");
+            if (username != "" && username != null) {
+                utils.setCookie("username", username, 365);
+            }
+        }
+    }
+    function showUsername (username) {
+        var welcomeMessage = "Welcome again " + username + "!";
+        document.getElementById("username").innerHTML = welcomeMessage;
+    }
+    checkCookie();
 
     var context_ = singletonContext.getInstance();
 
--- javascript/utils.js
+++ javascript/utils.js
@@ -1,5 +1,5 @@
 /**
- *  Used to avoid image double click selection default behaviour 
+ *  Used to avoid image double click selection default behaviour
  */
 function clearSelection() {
      if(document.selection && document.selection.empty) {
@@ -10,4 +10,32 @@ function clearSelection() {
      }
  }
 
- module.exports.clearSelection = clearSelection;
+function setCookie(cname, cvalue, exdays) {
+    if(exdays !== undefined){
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    } else {
+        document.cookie = cname + "=" + cvalue + ";";
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+module.exports.clearSelection = clearSelection;
+module.exports.setCookie = setCookie;
+module.exports.getCookie = getCookie; 
